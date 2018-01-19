62d96e153519fe78068c0468438f150ae745519e
Giovani Silva <giovani.m.silva@gmail.com>
2018-01-19 13:02:25 -0200

    Criado notificação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit, HostListener } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
 import { AppService } from './app.service';
+
+declare var Favico:any;
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,10 @@ export class AppComponent {
   chatMessages: Object[] = [];
   messageText: string;
   username: string;
+  connected: boolean;
+  notReadMsg: number = 0;
+  favico: any;
+  windowActivated: boolean;
 
   @ViewChild('bodyContainer') private bodyContainer: ElementRef;
 
@@ -21,28 +27,66 @@ export class AppComponent {
     this.appService.getUpdate().subscribe(data => this.updateChat(data));
   }
 
-  ngOnInit() {
+  @HostListener('window:focus', ['$event'])
+  onFocus(event: any): void {
+    this.windowActivated = true;
+    this.removeIconBadge();
+  }
 
-    this.username = prompt("Please enter your name");
-    this.appService.join(this.username);
+  @HostListener('window:blur', ['$event'])
+  onBlur(event: any): void {
+    this.windowActivated = false;
+  }
+
+  ngOnInit() {
+    this.favico = new Favico({
+        animation:'none'
+    });
+
+    this.username = prompt("Please enter your name:");
+    if(this.username !== null) {
+      this.connected = true;
+      this.appService.join(this.username);
+    } else {
+      this.connected = false;
+      this.chatMessages.push({msg: "Not connected, refresh the page and enter a name to connect to the chat", server: true});
+    }
   }
 
   sendMessage() {
-    this.appService.sendMessage(this.messageText);
-    this.updateChat({name: this.username, msg: this.messageText, external: false});
-    this.messageText = "";
+
+    if(this.messageText.length > 0) {
+      this.appService.sendMessage(this.messageText);
+      this.updateChat({name: this.username, msg: this.messageText, external: false});
+      this.messageText = "";
+    }
   }
 
   updateChat(msg) {
 
-    this.chatMessages.push(msg);
-    this.scrollToBottom();
+    if(this.connected) {
+      this.chatMessages.push(msg);
+      if(!this.windowActivated) this.addIconBadge();
+      this.scrollToBottom();
+    }
   }
 
   scrollToBottom(): void {
     try {
       Observable.timer(100).subscribe(() => this.bodyContainer.nativeElement.scrollTop = this.bodyContainer.nativeElement.scrollHeight);
     } catch(err) { }                 
+  }
+
+  addIconBadge() {
+    
+    this.notReadMsg += 1;
+    this.favico.badge(this.notReadMsg);
+  }
+
+  removeIconBadge() {
+
+    this.notReadMsg = 0;
+    this.favico.reset();
   }
 
 }
